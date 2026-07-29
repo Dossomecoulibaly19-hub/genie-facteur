a39e19fe46a1d39de5ac06d0b5f1a494670ff845
Dossomecoulibaly19-hub <kikicoulibaly19@gmail.com>
2026-07-29 07:43:33 +0000

    Update server.py

--- server.py
+++ server.py
@@ -3,9 +3,9 @@ from flask_socketio import SocketIO, emit, join_room, leave_room
 import os
 
 app = Flask(__name__)
-# threading = plus stable en local, eventlet = obligatoire sur Render
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet', logger=True, engineio_logger=True)
-USERS_ONLINE = {}
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+
+USERS_ONLINE = {} # {code: sid}
 
 @app.route('/')
 def home():
@@ -16,20 +16,19 @@ def handle_join(data):
     code = data['code'].upper()
     sid = request.sid
     USERS_ONLINE[code] = sid
-    join_room(sid) # CRITIQUE: pour pouvoir envoyer au sid
-    print(f"[JOIN] {code} connecté avec SID {sid}")
+    join_room(sid) # CRITIQUE pour envoyer au bon user
+    print(f"[JOIN] {code} connecté")
 
 @socketio.on('send_message')
 def handle_message(data):
     to_code = data['to'].upper()
-    print(f"[SEND] {data['from']} -> {to_code}")
-
     if to_code in USERS_ONLINE:
         target_sid = USERS_ONLINE[to_code]
         emit('receive_message', data, room=target_sid)
-        emit('new_message_alert', {}, room=target_sid) # pour reload l'app
+        emit('new_message_alert', {}, room=target_sid) # force reload app
+        print(f"[SEND] {data['from']} -> {to_code}")
     else:
-        print(f"[OFFLINE] {to_code} n'est pas connecté")
+        print(f"[OFFLINE] {to_code}")
 
 @socketio.on('disconnect')
 def handle_disconnect():
@@ -37,7 +36,6 @@ def handle_disconnect():
     for code, s in list(USERS_ONLINE.items()):
         if s == sid:
             del USERS_ONLINE[code]
-            leave_room(sid)
             print(f"[LEAVE] {code} déconnecté")
 
 if __name__ == '__main__':
